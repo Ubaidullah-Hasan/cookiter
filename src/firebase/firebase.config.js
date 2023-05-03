cf3e4cdff330bbcba62008c2fff9bca0250bccca
Ubaidullah-Hasan <hm2964133@gmail.com>
2023-05-03 07:43:52 +0600

    firebase i and secure

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBOALRqsUcfil9_W5iwy9kPJURxyW2_D_U",
-    authDomain: "cookiter-client-site.firebaseapp.com",
-    projectId: "cookiter-client-site",
-    storageBucket: "cookiter-client-site.appspot.com",
-    messagingSenderId: "11297559635",
-    appId: "1:11297559635:web:d41018e52f982d3e2ca2a6"
+    apiKey:import.meta.env.VITE_APIKEY ,
+    authDomain:import.meta.env.VITE_AUTHDOMAIN ,
+    projectId:import.meta.env.VITE_PROJECTID ,
+    storageBucket:import.meta.env.VITE_STORAGEBUCKET ,
+    messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID ,
+    appId:import.meta.env.VITE_APPID ,
 };
 
 // Initialize Firebase
